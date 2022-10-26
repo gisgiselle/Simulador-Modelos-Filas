29ac5a622bf73588cfdb69ff5bd233a662e7e2ce
gisgiselle <brengimolina@gmail.com>
2022-10-26 16:29:02 -0500

    MMD1 falta pruebas

---
+++ Simuladores/MD1_calc.py
@@ -0,0 +1,27 @@
+results = {
+    "lambda": 0.0,
+    "miu": 0.0,
+    "s": 1,
+    "rho": 0.0,
+    "p0": 0.0,
+    "Lq": 0.0,
+    "L": 0.0,
+    "Wq": 0.0,
+    "W": 0.0,
+}
+
+def calcular(tasa_llegadas,tasa_servicios):
+    results["lambda"] = tasa_llegadas
+    results["miu"] = tasa_servicios
+
+    results["rho"] = results["lambda"] / results["miu"]
+
+    results["p0"] = 1 - results["rho"]
+
+    results["Lq"] = (results["rho"] ** 2) / (2 * (1 - results["rho"]))
+
+    results["Wq"] = results["Lq"] / results["lambda"]
+
+    results["W"] = results["Wq"] + (1 / results["miu"])
+
+    return results
