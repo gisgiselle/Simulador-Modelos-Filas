a3bf5056bedee83b0675c706a42c0939acaec89a
gisgiselle <brengimolina@gmail.com>
2022-10-27 08:36:07 -0500

    ME1 listo faltan pruebas

---
+++ Simuladores/ME1_calc.py
@@ -0,0 +1,33 @@
+results = {
+    "lambda": 0.0,
+    "miu": 0.0,
+    "s": 0,
+    "k": 0.0,
+    "rho": 0.0,
+    "p0": 0.0,
+    "Lq": 0.0,
+    "L": 0.0,
+    "Wq": 0.0,
+    "W": 0.0,
+}
+
+def calcular(tasa_llegada, tasa_servicios, k):
+    results["lambda"] = tasa_llegada
+    results["miu"] = tasa_servicios
+    results["k"] = k
+    results["s"] = 1
+
+    results["rho"] = results["lambda"] / results["miu"]
+
+    results["p0"] = 1 - results["rho"]
+
+    results["Lq"] = ((1+results["k"]) / (2 * results["k"])) * (results["lambda"])**2 / \
+                    (results["miu"] * (results["miu"]-results["lambda"]))
+
+    results["Wq"] = results["Lq"] / results["lambda"]
+
+    results["W"] = results["Wq"] + (1 / results["miu"])
+
+    results["L"] = results["lambda"] * results["W"]
+
+    return results
