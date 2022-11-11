bfcedd74fd5315030e32d3c61151d1ed685778d5
gisgiselle <brengimolina@gmail.com>
2022-11-11 12:52:30 -0600

    agraedo n

--- Simuladores/MEs_calc.py
+++ Simuladores/MEs_calc.py
@@ -5,8 +5,10 @@ results = {
     "lambda": 0.0,
     "miu": 0.0,
     "s": 0,
+    "n": 0,
     "k": 0.0,
     "rho": 0.0,
+    "pn": 0.0,
     "p0": 0.0,
     "Lq": 0.0,
     "L": 0.0,
@@ -14,15 +16,18 @@ results = {
     "W": 0.0,
 }
 
-def calcular(tasa_llegada, tasa_servicios, k,s):
+def calcular(tasa_llegada, tasa_servicios, k,s,n):
     results["lambda"] = tasa_llegada
     results["miu"] = tasa_servicios
     results["k"] = k
     results["s"] = s
+    results["n"] = n
 
     results["rho"] = results["lambda"] / (results["s"]*results["miu"])
 
     results["p0"] = 1 - results["rho"]
+
+    results["pn"] = (results["rho"]**results["n"]) * results["p0"]
 
     results["Lq"] = ((1+results["k"]) / (2 * results["k"])) * (results["lambda"])**2 / \
                     (results["miu"] * (results["miu"]-results["lambda"]))
@@ -36,4 +41,4 @@ def calcular(tasa_llegada, tasa_servicios, k,s):
     return results
 
 
-pprint.pprint(calcular(1,2,1,1))
+pprint.pprint(calcular(1,2,1,1,100))
