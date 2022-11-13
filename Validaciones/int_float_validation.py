1afdf10dd9a5d82e3bc581e931fbc882f6d4ffdb
DVilla2397 <49038173+DVilla2397@users.noreply.github.com>
2022-11-12 20:09:01 -0600

    Correcion Validacion

--- Validaciones/int_float_validation.py
+++ Validaciones/int_float_validation.py
@@ -8,15 +8,11 @@ def inputFloat(inputUsuario):
         else:
             othstr = newstr.replace(" ", "")
             ytr = othstr.isalpha()
-            print("Es una palabra: ")
-            print(ytr)
             if ytr == True:
                 return print("No se puede, ERROR")
             elif ytr == False:
-                print("continuamos...")
                 nnewstr = newstr[:sstr] + '.' + newstr[sstr:]
                 nowfloat = float(nnewstr)
-                print(nowfloat)
                 return nowfloat
     else:
         return print("ERROR, introduzca un valor valido")
@@ -25,21 +21,13 @@ def inputInt(inputUsuario2):
     str2 = inputUsuario2
     #Empieza proceso, no hay punto, revisar si es una palabra. (Analiza que no sea un valor negativo).
     xtr = str2.replace(" ", "")
-    print(xtr)
     if xtr.find("-")!=-1:
         print("Es un valor negativo, ERROR")
     else:
         nwstr = xtr.isalpha()
-        print("Es una palabra: ")
-        print(nwstr)
     if nwstr == True:
         print("No se puede")
     elif nwstr == False:
-        print("continuamos")
-        print(type(xtr))
         nowint = int(xtr)
-        print(nowint)
-        print(type(nowint))
-    
     return nowint
   #Termina proceso, no hay punto, revisar si es una palabra. (Analiza que no sea un valor negativo).
